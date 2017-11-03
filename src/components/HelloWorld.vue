10e71c927c0e82f43b8b027bc26508a2de856649
WuXueyou <409898859@qq.com>
2017-11-03 08:07:17 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+  <el-container style="margin: 0;padding: 0;">
+    <el-header>asdf</el-header>
+    <el-container>
+      <el-aside width="200px" class="border2">Aside</el-aside>
+      <el-main class="border3">Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+  .el-container {
+    height: 100%;
+  }
+
+  .border1 {
+    border: 2px solid green;
+  }
+
+  .border2 {
+    border: 2px solid red;
+  }
+
+  .border3 {
+    border: 2px solid blue;
+  }
+
+  .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 50px;
+    height: 50px;
+  }
+</style>
